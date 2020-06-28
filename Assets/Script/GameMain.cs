70cd74ea63c10b3ec38f9a020e6568a5935666d9
KingJA <kingjavip@gmail.com>
2020-06-28 17:23:58 +0800

    first

---
+++ Assets/Script/GameMain.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameMain : MonoBehaviour
+{
+
+   public Button btnLevel1;
+   public Button btnLevel2;
+   public Button btnLevel3;
+
+   public Transform panelStart;
+   public Transform panelCard;
+   public Transform panelOver;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnLevel1.onClick.AddListener(()=>{
+            panelStart.gameObject.SetActive(false);
+            panelCard.gameObject.SetActive(true);
+            initCard(2,3);
+        });
+        btnLevel2.onClick.AddListener(()=>{
+            panelStart.gameObject.SetActive(false);
+            panelCard.gameObject.SetActive(true);
+             initCard(2,4);
+        });
+        btnLevel3.onClick.AddListener(()=>{
+            panelStart.gameObject.SetActive(false);
+            panelCard.gameObject.SetActive(true);
+             initCard(2,5);
+        });
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void initCard(int row,int col){
+      Sprite[] sps=  Resources.LoadAll<Sprite>("");
+
+      for (int i = 0; i < sps.Length; i++)
+      {
+          Debug.LogError(sps[i].name);
+      }
+    }
+}
