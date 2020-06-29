5c28c2e64d19f7c98a6c72584a0414e0e745c60d
KingJA <kingjavip@gmail.com>
2020-06-29 17:19:13 +0800

    5

--- Assets/Script/CardFlipAnim.cs
+++ Assets/Script/CardFlipAnim.cs
@@ -1,18 +1,126 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CardFlipAnim : MonoBehaviour
-{
+public class CardFlipAnim : MonoBehaviour, IPointerClickHandler {
+    Transform cardFront;
+    Transform cardBack;
+    float flipDuaration = 0.2f;
+    public bool isOnFront = false;
+    public bool isOver = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+
+    }
+
+     void Awake(){
+         cardFront = transform.Find("card_front");
+         cardBack = transform.Find("card_back");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+    }
+
+    public void OnPointerClick(PointerEventData eventData){
+        // Debug.LogError("点击");
+        if (isOnFront) {
+            StartCoroutine(FlipCardToBack());
+        }
+        else {
+            StartCoroutine(FlipCardToFront());
+        }
+    }
+
+    IEnumerator FlipCardToFront(){
+        //1.翻转反面到90度
+        cardFront.gameObject.SetActive((false));
+        cardBack.gameObject.SetActive((true));
+        cardFront.rotation = Quaternion.identity;
+        while (cardBack.rotation.eulerAngles.y < 90) {
+            cardBack.rotation *= Quaternion.Euler(0, Time.deltaTime * 90f * (1f / flipDuaration), 0);
+            if (cardBack.rotation.eulerAngles.y > 90) {
+                cardBack.rotation = Quaternion.Euler(0, 90f, 0);
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        //2.翻转正面到0度
+        cardFront.gameObject.SetActive((true));
+        cardBack.gameObject.SetActive((false));
+        cardFront.rotation = Quaternion.Euler(0, 90, 0);
+        while (cardFront.rotation.eulerAngles.y > 0) {
+            cardFront.rotation *= Quaternion.Euler(0, -Time.deltaTime * 90f * (1f / flipDuaration), 0);
+            if (cardFront.rotation.eulerAngles.y > 90) {
+                cardFront.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        isOnFront = true;
+        Camera.main.gameObject.GetComponent<GameMain>().checkIsGameOver();
+    }
+
+    IEnumerator FlipCardToBack(){
+        //1.翻转反面到90度
+        cardBack.gameObject.SetActive((false));
+        cardFront.gameObject.SetActive((true));
+        cardBack.rotation = Quaternion.identity;
+        while (cardFront.rotation.eulerAngles.y < 90) {
+            cardFront.rotation *= Quaternion.Euler(0, Time.deltaTime * 90f * (1f / flipDuaration), 0);
+            if (cardFront.rotation.eulerAngles.y > 90) {
+                cardFront.rotation = Quaternion.Euler(0, 90f, 0);
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        //2.翻转正面到0度
+        cardBack.gameObject.SetActive((true));
+        cardFront.gameObject.SetActive((false));
+        cardBack.rotation = Quaternion.Euler(0, 90, 0);
+        while (cardBack.rotation.eulerAngles.y > 0) {
+            cardBack.rotation *= Quaternion.Euler(0, -Time.deltaTime * 90f * (1f / flipDuaration), 0);
+            if (cardBack.rotation.eulerAngles.y > 90) {
+                cardBack.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        isOnFront = false;
+    }
+
+    internal string getCardImageName(){
+        return cardFront.GetComponent<Image>().sprite.name;
+    }
+
+    public void mathSuccess(){
+        isOver = true;
+        cardFront.gameObject.SetActive(false);
+        cardBack.gameObject.SetActive(false);
+    }
+
+    public void matchFail(){
+        StartCoroutine(FlipCardToBack());
+    }
+
+    public void setDefaultStatus(){
+       cardFront.gameObject.SetActive(false);
+       cardBack.gameObject.SetActive(true);
+       this.isOver = false;
+       this.isOnFront = false;
+       cardFront.rotation=Quaternion.identity;
+       cardBack.rotation=Quaternion.identity;
     }
 }
--- Assets/Script/GameMain.cs
+++ Assets/Script/GameMain.cs
@@ -1,55 +1,154 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class GameMain : MonoBehaviour {
+    public Button btnLevel1;
+    public Button btnLevel2;
+    public Button btnLevel3;
 
-public class GameMain : MonoBehaviour
-{
-
-   public Button btnLevel1;
-   public Button btnLevel2;
-   public Button btnLevel3;
-
-   public Transform panelStart;
-   public Transform panelCard;
-   public Transform panelOver;
-
+    public Transform panelStart;
+    public Transform panelCard;
+    public Transform panelOver;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        btnLevel1.onClick.AddListener(()=>{
+    void Start(){
+        btnLevel1.onClick.AddListener(() => {
             panelStart.gameObject.SetActive(false);
             panelCard.gameObject.SetActive(true);
-            initCard(2,3);
+            initCard(2, 3);
         });
-        btnLevel2.onClick.AddListener(()=>{
+        btnLevel2.onClick.AddListener(() => {
             panelStart.gameObject.SetActive(false);
             panelCard.gameObject.SetActive(true);
-             initCard(2,4);
+            initCard(2, 4);
         });
-        btnLevel3.onClick.AddListener(()=>{
+        btnLevel3.onClick.AddListener(() => {
             panelStart.gameObject.SetActive(false);
             panelCard.gameObject.SetActive(true);
-             initCard(2,5);
+            initCard(2, 5);
         });
-        
+        Button btnRestart = panelOver.Find("btn_restart").GetComponent<Button>();
+        btnRestart.onClick.RemoveAllListeners();
+        btnRestart.onClick.AddListener(goGameStartPage);
+
+        Button btnReturn = panelCard.Find("btn_home").GetComponent<Button>();
+        btnReturn.onClick.RemoveAllListeners();
+        btnReturn.onClick.AddListener(goGameStartPage);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
     }
 
-    void initCard(int row,int col){
-      Sprite[] sps=  Resources.LoadAll<Sprite>("");
+    void initCard(int row, int col){
+        //1.加载卡牌图片
+        Debug.LogError("row："+row+" col："+col);
+        Sprite[] sps = Resources.LoadAll<Sprite>("");
+        //2.计算需要加载卡牌的数量
+        int totalCount = row * col / 2;
 
-      for (int i = 0; i < sps.Length; i++)
-      {
-          Debug.LogError(sps[i].name);
-      }
+        Debug.LogError("计划加载："+totalCount);
+        //3.计算随机加载卡牌的索引
+        List<Sprite> spsList = new List<Sprite>();
+        for (int i = 0; i < sps.Length; i++) {
+            spsList.Add((sps[i]));
+        }
+
+        List<Sprite> needShowCardList = new List<Sprite>();
+        while (totalCount > 0) {
+            int randomIndex = Random.Range(0, spsList.Count);
+            needShowCardList.Add(spsList[randomIndex]);
+            needShowCardList.Add(spsList[randomIndex]);
+            spsList.RemoveAt(randomIndex);
+            totalCount--;
+        }
+
+        for (int i = 0; i < needShowCardList.Count; i++) {
+            // Debug.LogError(needShowCardList[i].name);
+        }
+
+        //4.显示卡牌到UI上
+        Transform contentRoot = panelCard.Find("Panel");
+        // int maxCount = Mathf.Max(contentRoot.childCount, needShowCardList.Count);
+        int maxCount = Mathf.Max(contentRoot.childCount, needShowCardList.Count);
+        GameObject itemPrefab = contentRoot.GetChild(0).gameObject;
+        Debug.LogError("maxCount："+maxCount);
+
+        for (int i = 0; i < maxCount; i++) {
+            GameObject itemObject = null;
+            if (i < contentRoot.childCount) {
+                itemObject = contentRoot.GetChild(i).gameObject;
+            }
+            else {
+                itemObject = GameObject.Instantiate<GameObject>(itemPrefab);
+                itemObject.transform.SetParent(contentRoot, false);
+            }
+            Debug.LogError("index："+i);
+            itemObject.transform.Find("card_front").GetComponent<Image>().sprite = needShowCardList[i];
+            CardFlipAnim cardFlipAnim = itemObject.GetComponent<CardFlipAnim>();
+            cardFlipAnim.setDefaultStatus();
+        }
+
+        GridLayoutGroup glg = contentRoot.GetComponent<GridLayoutGroup>();
+
+        float panelWidth = col * glg.cellSize.x + glg.padding.left + glg.padding.right + (col - 1) * glg.spacing.x;
+        float panelHeight = row * glg.cellSize.y + glg.padding.top + glg.padding.bottom + (row - 1) * glg.spacing.y;
+        contentRoot.GetComponent<RectTransform>().sizeDelta = new Vector2(panelWidth, panelHeight);
+    }
+
+    public void checkIsGameOver(){
+        Debug.LogError("checkIsGameOver");
+        CardFlipAnim[] allCards = GameObject.FindObjectsOfType<CardFlipAnim>();
+
+        if (allCards != null && allCards.Length > 0) {
+            List<CardFlipAnim> cardInFront = new List<CardFlipAnim>();
+            for (int i = 0; i < allCards.Length; i++) {
+                CardFlipAnim cardTem = allCards[i];
+                if (cardTem.isOnFront && !cardTem.isOver) {
+                    cardInFront.Add(cardTem);
+                }
+
+                if (cardInFront.Count >= 2) {
+                    string cardImageName1 = cardInFront[0].getCardImageName();
+                    string cardImageName2 = cardInFront[1].getCardImageName();
+                    if (cardImageName1 == cardImageName2) {
+                        cardInFront[0].mathSuccess(); //标记为匹配结束
+                        cardInFront[1].mathSuccess(); //标记为匹配结束
+                    }
+                    else {
+                        cardInFront[0].matchFail(); //翻转到反面
+                        cardInFront[1].matchFail(); //翻转到反面
+                    }
+
+                    bool isAllOver = true;
+                    for (int j = 0; j < allCards.Length; j++) {
+                        isAllOver &= allCards[j].isOver;
+
+                    }
+
+                    if (isAllOver) {
+                        goGameOverPage();
+                    }
+
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private void goGameOverPage(){
+        panelStart.gameObject.SetActive(false);
+        panelCard.gameObject.SetActive(false);
+        panelOver.gameObject.SetActive(true);
+    }
+
+    private void goGameStartPage(){
+        panelStart.gameObject.SetActive(true);
+        panelCard.gameObject.SetActive(false);
+        panelOver.gameObject.SetActive(false);
     }
 }
